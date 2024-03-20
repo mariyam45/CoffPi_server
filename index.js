98f61ae9f0e839d083d287ac8167fbb6f989edd0
mariyam45 <mariansuchi019915gmail.com>
2024-03-21 03:28:25 +0600

    api updated v2

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
 const chefs = require("./data/chefs.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(" coffee shop open");
@@ -10,6 +14,20 @@ app.get("/", (req, res) => {
 app.get("/chefs", (req, res) => {
   res.send(chefs);
 });
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  // console.log(id);
+  const selectedChefs = chefs.find((n) => n.id == id);
+  res.send(selectedChefs);
+});
+
+// app.get("/chefs/:id/:recipes", (req, res) => {
+//   const recipes = req.params.recipes;
+//   // console.log(recipes);
+//   const allRecipes = find((n) => n.recipes == recipes);
+//   res.send(allRecipes);
+// });
 
 app.listen(port, () => {
   console.log(`coffee shop on port : ${port}`);
